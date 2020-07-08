2d97b5f4f717c5e1d8f2658dfe5f16bb5a39b253
Freakly <39679911+d3d9god@users.noreply.github.com>
2020-07-08 11:29:35 +0300

    Add files via upload

---
+++ csgo.h
@@ -0,0 +1,82 @@
+class GayVars_
+{
+public:
+
+	DWORD pOwner = 0x29CC;
+
+	DWORD m_Collision = 0x320;
+	DWORD m_vecOrigin = 0x138;
+	DWORD m_vecViewOffset = 0x108;
+	DWORD m_bIsPlayerGhost = 0;
+	DWORD m_vecPunchAngles = 0x302C; // aim olarak da geçer  // m_aimPunchAngle 
+	DWORD m_vecViewPunchAngles = 0x3020;  // m_viewPunchAngle
+	DWORD m_flSimulationTime = 0x268;
+	DWORD m_vecVelocity = 0x114;
+	DWORD m_bIsScoped = 0x3928;
+	DWORD m_lifeState = 0x25F;
+	DWORD m_vecAngles = 0;
+
+	DWORD m_fFlags = 0x104;
+	DWORD m_iHealth = 0x100;
+	DWORD m_bGunGameImmunity = 0x3944;
+	DWORD m_iTeamNum = 0xF4;
+	DWORD m_iGlowIndex = 0xA438;
+	DWORD m_iShotsFired = 0xA390;
+	DWORD m_flFlashMaxAlpha = 0xA41C;
+	DWORD m_hActiveWeapon = 0x2EF8;
+	DWORD m_hMyWeapons = 0x2DF8;
+	DWORD m_nHitboxSet = 0;
+	DWORD m_flFlashDuration = 0xA420;
+	DWORD m_iMoney = 0;
+	DWORD m_iScore = 0;
+	DWORD m_nModelIndex = 0; // olmadı 0
+	DWORD m_hOwner = 0x29CC;
+	DWORD m_hWeapon = 0; //0x2DF8;
+	DWORD m_hViewModel = 0;
+	DWORD m_nTickBase = 0x3430;
+	DWORD m_flMaxspeed = 0;
+	DWORD m_hObserverTarget = 0x338C;
+	DWORD m_flC4Blow = 0x2990;
+	DWORD m_flTimerLength = 0x2994;
+	DWORD m_hMyWearables = 0;
+	DWORD m_angEyeAngles = 0;
+
+	DWORD m_iAccountID = 0x2FC8;
+	DWORD m_iItemDefinitionIndex = 0x2FAA;
+	DWORD m_iItemIDHigh = 0x2FC0;
+	DWORD m_iEntityQuality = 0x2FAC;
+	DWORD m_szCustomName = 0x303C;
+	DWORD m_nFallbackPaintKit = 0x31B8;
+	DWORD m_flFallbackWear = 0x31C0;
+	DWORD m_nFallbackSeed = 0x31BC;
+	DWORD m_nFallbackStatTrak = 0x31C4;
+	DWORD m_OriginalOwnerXuidLow = 0x31B0;
+	DWORD m_OriginalOwnerXuidHigh = 0x31B4;
+
+	//Weapons
+	DWORD m_flNextPrimaryAttack = 0x3238;
+	DWORD m_fAccuracyPenalty = 0x3330;
+	DWORD m_flNextSecondaryAttack = 0;
+	DWORD m_iClip1 = 0x3264;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
